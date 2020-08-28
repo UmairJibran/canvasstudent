1711d8033f9e32b3165876edb1694e23b8eb6ad5
Umair Jibran <umairjibran7@gmail.com>
2020-08-28 07:23:57 +0500

    fetched data on first screen

--- lib/screens/home_screens/dashboard.dart
+++ lib/screens/home_screens/dashboard.dart
@@ -1,11 +1,54 @@
-import 'package:canvasstudent/data/dummy.dart';
 import 'package:canvasstudent/widgets/home_grid_tile.dart';
 import 'package:canvasstudent/widgets/home_list_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  bool loadingSubjects = true;
+  bool loadingGroups = true;
+  List<Map<String, dynamic>> subjects = [];
+  List<Map<String, dynamic>> studentGroups = [];
+
+  void loadSubjects() async {
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    var snapshot = firebaseFirestore.collection("subjects").get();
+    await snapshot.then((value) {
+      value.docs.forEach((element) {
+        var subject = element.data();
+        subjects.add(subject);
+      });
+    }).then((_) {
+      setState(() {
+        loadingSubjects = false;
+      });
+    });
+  }
+
+  void loadGroups() async {
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    var snapshot = firebaseFirestore.collection("student_groups").get();
+    await snapshot.then((value) {
+      var docs = value.docs;
+      docs.forEach((element) {
+        var group = element.data();
+        studentGroups.add(group);
+      });
+    }).then((_) {
+      setState(() {
+        loadingGroups = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    loadSubjects();
+    loadGroups();
     return Container(
       height: MediaQuery.of(context).size.height * 0.8,
       width: MediaQuery.of(context).size.width,
@@ -34,27 +77,28 @@ class Dashboard extends StatelessWidget {
               ),
             ],
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.226,
-            width: MediaQuery.of(context).size.width * 0.9,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                HomeGridTile(
-                  color: subjects[0]["color"],
-                  subject: subjects[0]["subjectName"],
-                  subjectCode: subjects[0]["subjectCode"],
-                  subjectID: 0,
+          loadingSubjects
+              ? CircularProgressIndicator()
+              : Container(
+                  height: MediaQuery.of(context).size.height * 0.226,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: ListView.separated(
+                    separatorBuilder: (_, __) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                      );
+                    },
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      return HomeGridTile(
+                        color: Colors.blue,
+                        subject: subjects[index]["subjectName"],
+                        subjectCode: subjects[index]["subjectCode"],
+                      );
+                    },
+                    itemCount: subjects.length,
+                  ),
                 ),
-                HomeGridTile(
-                  color: subjects[1]["color"],
-                  subject: subjects[1]["subjectName"],
-                  subjectCode: subjects[1]["subjectCode"],
-                  subjectID: 1,
-                ),
-              ],
-            ),
-          ),
           SizedBox(
             height: 12,
           ),
@@ -65,31 +109,27 @@ class Dashboard extends StatelessWidget {
               fontSize: 28,
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.4,
-            child: ListView(
-              children: [
-                HomeListTile(
-                  color: subjects[0]["color"],
-                  subjectTitle: subjects[0]["subjectName"],
-                  groupFor: "Term 2",
-                  groupName: "New Students",
+          loadingGroups
+              ? CircularProgressIndicator()
+              : Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: ListView.separated(
+                    itemBuilder: (_, index) {
+                      return HomeListTile(
+                        color: Colors.amber,
+                        groupFor: studentGroups[index]["groupFor"],
+                        groupName: studentGroups[index]["groupName"],
+                        subjectTitle: studentGroups[index]["subjectTitle"],
+                      );
+                    },
+                    separatorBuilder: (_, __) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                      );
+                    },
+                    itemCount: studentGroups.length,
+                  ),
                 ),
-                HomeListTile(
-                  color: subjects[1]["color"],
-                  subjectTitle: subjects[1]["subjectName"],
-                  groupFor: "Term 3",
-                  groupName: "New Students",
-                ),
-                HomeListTile(
-                  color: subjects[2]["color"],
-                  subjectTitle: subjects[2]["subjectName"],
-                  groupFor: "Term 1",
-                  groupName: "New Students",
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
